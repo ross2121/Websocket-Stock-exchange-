23db088bdd27c3c5f783b61f439aa5d83e6b710f
ross2121 <youvalsingh40@gmail.com>
2025-02-13 15:10:45 +0530

    Websocket ready to deploy

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { WebSocketServer } from "ws";
 import { UserManager } from "./UserManager";
 import {createClient} from "redis"
-const client=createClient();
-client.connect();
-console.log("connect to redddis");
+
+async function redis(){
+    const client=createClient({
+    url:"rediss://red-cumrnjd6l47c7397qhf0:9ybkrbUwo8EZJraRqJ15mAINVUSNmEWc@oregon-redis.render.com:6379"
+});
+try{await client.connect();
+    console.log("connect to redddis");
+}catch(e){
+    console.log(e);
+}
+
+}
+redis();
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on("connection", (ws) => {
